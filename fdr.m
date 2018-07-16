fd95c9f64ad9e72bd542814aaba4cc2fe3047e1b
'mpdannenberg' <'dannenberg@email.arizona.edu'>
2018-07-16 11:21:35 -0700

    Remove "for" loop

--- fdr.m
+++ fdr.m
@@ -3,15 +3,13 @@ function [ test, pfdr ] = fdr( ps, q )
 %   See Johnson 2013 (J. Clim.), pp. 4819-4820
 %   See Wilks 2006 (J. Appl. Meteorol. Clim.)
 
-if ~exist('q','var'), q = 0.05; end;
+if ~exist('q','var'), q = 0.05; end
 M = length(ps);
 ps = sort(ps);
 
-pm = NaN(1,M);
-for m = 1:M
-    qtest = q * (m/M);
-    pm(m) = ps(m) <= qtest;
-end
+m = 1:M;
+qtest = q * (m/M);
+pm = ps <= qtest;
 
 pfdr = max(ps(pm == 1));
 test = max(pm);

93d61ff96657989abbc29f718868362556af494e
mdannenberg <mdannenberg@172.17.49.61>
2019-04-09 11:07:42 -0500

    Initlal commit

---
+++ dberg_box.m
@@ -0,0 +1,23 @@
+function [h] = dberg_box(x,data,EdgeColor,BoxColor,width)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+md = median(data);
+qs = quantile(data, [0.25 0.75]);
+iqr = diff(qs);
+oidx = data > (1.5*iqr + max(qs)) | data < (min(qs) - 1.5*iqr);
+outs = data(oidx);
+notouts = data(~oidx);
+
+plot([x x], [min(notouts) max(notouts)], 'LineWidth',2, 'Color',EdgeColor)
+hold on;
+fill([x-width/2 x+width/2 x+width/2 x-width/2], [min(qs) min(qs) max(qs) max(qs)],...
+    BoxColor, 'EdgeColor',EdgeColor, 'LineWidth',2);
+plot([x-width/2 x+width/2], [md md], 'LineWidth',2, 'Color',EdgeColor);
+
+if ~isempty(outs)
+    plot(repmat(x, size(outs)), outs, 'o', 'LineWidth',2, 'Color',EdgeColor);
+end
+
+end
+
